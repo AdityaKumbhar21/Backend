480f71975ad0fe43a9b1888b1c055a33620c2cac
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-02-21 16:29:41 +0530

    updated app.js

--- DataAssociation/app.js
+++ DataAssociation/app.js
@@ -20,16 +20,21 @@ app.get('/create',async (req, res) => {
 // 67b85b713a6fc8deafac08fd
 
 app.get('/createPost',async (req, res) => {
+    
+    //creating a post.
     const createdPost = await post.create({
         postData: "This is post data",
         user: "67b85b713a6fc8deafac08fd"
     });
 
+    // finding the user of the created post
     const currUser = await user.findOne({_id:"67b85b713a6fc8deafac08fd"});
+    // pushing the created post in the posts array of the user
     currUser.posts.push(createdPost._id);
+    // saving the userData
     await currUser.save();
     res.send({createdPost, currUser});
-})
+});
 
 
 
