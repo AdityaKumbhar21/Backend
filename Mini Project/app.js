de585dbfebc3164c52ffe9c5e60ee559b8e73276
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-03-06 18:49:29 +0530

    set up the mini project

---
+++ Mini Project/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
+const app = express();
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname + "public")));
+
+app.get('/',(req, res)=>{
+    res.render("index");
+});
+
+
+app.listen(3000);
