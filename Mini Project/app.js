2e0fecf637a7f17b40c366f412947a3a1291532e
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-03-07 10:17:07 +0530

    done with authentication and protected route mw

--- Mini Project/app.js
+++ Mini Project/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
 import userModel from "./models/user.js";
+import postModel from "./models/post.js";
 import cookieParser from "cookie-parser";
 import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+const jwt = jsonwebtoken;
 
 
 const app = express();
@@ -18,6 +20,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname + "public")));
 app.use(cookieParser());
 
+
 app.get('/',(req, res)=>{
     res.render("index");
 });
@@ -27,6 +30,28 @@ app.get('/login',(req, res)=>{
     res.render('login')
 });
 
+app.post('/login',async (req, res)=>{
+    const {username, password} = req.body;
+    
+    const user = await userModel.findOne({username});
+    if(user){
+        bcrypt.compare(password, user.password, (err, result)=>{
+            if(err){
+                res.send("505 Server error");
+            }
+            if(!result){
+                res.send("Password is incorrect");
+            }
+            else{
+                const token = jwt.sign({name:user.name, username: user.username, email:user.email, userid:user._id}, "secretKey");
+                res.cookie("token", token);
+                res.redirect('/home');
+            }
+        });
+    }
+    
+});
+
 app.post('/register',async (req, res)=>{
     const {name, email, username, password} = req.body;
     
@@ -34,8 +59,8 @@ app.post('/register',async (req, res)=>{
     if(!user){
         bcrypt.hash(password, 10,async  function(err, hash) {
             if(err){
-                console.log(err);
-                
+                res.send("505: Server Error")
+                console.log(err);  
             }
             const createdUser =  await userModel.create({
                 name,
@@ -43,11 +68,38 @@ app.post('/register',async (req, res)=>{
                 username,
                 password: hash
             });
-            res.redirect('/')
+
+            const token = jwt.sign({name, email,username,userid: createdUser._id},"secretKey");
+            res.cookie("token", token);
+            res.redirect('/home')
         });
     }
     
+});
+
+app.get('/home',isLoggedIn,(req, res)=>{
+    res.render('home');
+});
+
+app.get('/logout',(req, res)=>{
+    res.cookie('token',"");
+    res.redirect('/');
+});
+
+app.get('/error', (req, res)=>{
+    res.render("error");
 })
 
+
+function isLoggedIn(req, res, next){
+    if(req.cookies.token === ""){
+        res.redirect("/error");
+    }
+    else{
+        const data = jwt.verify(req.cookies.token , "secretKey");
+        console.log(data);
+        next();
+    }
+}
 
 app.listen(3000);
